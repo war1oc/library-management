2b833a50508733910d1276fd41570a6846cca5ca
Tanveer Hassan <tanveer_hassan@outlook.com>
2016-10-30 13:19:42 +0600

    renders flash messages and allows to delete them

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+
 import NavigationBar from './NavigationBar';
+import FlashMessagesList from './flash/FlashMessagesList';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
         <NavigationBar />
+        <FlashMessagesList />
         {this.props.children}
       </div>
     );
